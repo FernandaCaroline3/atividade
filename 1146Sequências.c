a8735d5616d9fda20418221a0d2004095de35d62
Fernanda <oliveira35carol.fcfo@gmail.com>
2024-09-25 19:04:31 -0300

    Add files via upload

---
+++ 1146Sequências.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int X = 1;
+
+    while (X != 0) {
+        scanf("%d", &X);
+
+        if (X != 0) {
+            for (int i = 1; i <= X; i++) {
+                if (i > 1) {
+                    printf(" ");
+                }
+                printf("%d", i);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+} 
